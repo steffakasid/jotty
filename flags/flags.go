679b754251e18b583526af7e9898ccb8ab3b738b
steffakasid <github@steffen-rumpf.de>
2024-03-24 22:58:11 +0100

    feat: Add parseUnverified if no signing-key was provided

---
+++ flags/flags.go
@@ -0,0 +1,23 @@
+package s
+
+const (
+	File        = "file"
+	NoHeader    = "no-header"
+	NoPayload   = "no-payload"
+	NoSignature = "no-signature"
+	Version     = "version"
+	Help        = "help"
+
+	WithAudience           = "with-audience"
+	WithExpirationRequired = "with-expiration"
+	WithIssuedAt           = "with-issued-at"
+	WithIssuer             = "with-issuer"
+	WithJSONNumber         = "with-json-number"
+	WithLeeway             = "with-leeway"
+	WithPaddingAllowed     = "with-padding-allowed"
+	WithStrictDecoding     = "with-strict-decoding"
+	WithSubject            = "with-subject"
+	WithValidMethod        = "with-valid-methods"
+
+	KeyFile = "key-file"
+)
--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/mattn/go-colorable"
-	json "github.com/neilotoole/jsoncolor"
-	flag "github.com/spf13/pflag"
+	"github.com/neilotoole/jsoncolor"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"github.com/steffakasid/eslog"
-	"github.com/steffakasid/jotty/internal"
+	flags "github.com/steffakasid/jotty/flags"
 	jotty "github.com/steffakasid/jotty/internal"
 )
 
@@ -19,52 +19,30 @@ const version = "0.1-development"
 
 var conf = &jotty.Config{}
 
-const (
-	fileFlag        = "file"
-	noHeaderFlag    = "no-header"
-	noPayloadFlag   = "no-payload"
-	noSignatureFlag = "no-signature"
-	versionFlag     = "version"
-	helpFlag        = "help"
-
-	withAudienceFlag           = "with-audience"
-	withExpirationRequiredFlag = "with-expiration"
-	withIssuedAtFlag           = "with-issued-at"
-	withIssuerFlag             = "with-issuer"
-	withJSONNumberFlag         = "with-json-number"
-	withLeewayFlag             = "with-leeway"
-	withPaddingAllowedFlag     = "with-padding-allowed"
-	withStrictDecodingFlag     = "with-strict-decoding"
-	withSubjectFlag            = "with-subject"
-	withValidMethodFlag        = "with-valid-methods"
-
-	keyFileFlag = "key-file"
-)
-
 func init() {
 	eslog.Logger.SetOutput(os.Stderr)
 
-	flag.StringP(fileFlag, "f", "", "Read token from  given file or if '-' from stdin.")
-	flag.BoolP(noHeaderFlag, "h", false, "Don't print the header of the JWT.")
-	flag.BoolP(noPayloadFlag, "p", false, "Don't print the payload of the JWT.")
-	flag.BoolP(noSignatureFlag, "s", false, "Don't print the signature of the JWT.")
-	flag.BoolP(versionFlag, "v", false, "Print version information.")
-	flag.BoolP(helpFlag, "?", false, "Print usage information.")
+	pflag.StringP(flags.File, "f", "", "Read token from  given file or if '-' from stdin.")
+	pflag.BoolP(flags.NoHeader, "h", false, "Don't print the header of the JWT.")
+	pflag.BoolP(flags.NoPayload, "p", false, "Don't print the payload of the JWT.")
+	pflag.BoolP(flags.NoSignature, "s", false, "Don't print the signature of the JWT.")
+	pflag.BoolP(flags.Version, "v", false, "Print version information.")
+	pflag.BoolP(flags.Help, "?", false, "Print usage information.")
 
-	flag.String(withAudienceFlag, "", "Configures the validator to require the specified audience in the `aud` claim.")
-	flag.Bool(withExpirationRequiredFlag, false, "This makes the exp claim required. By default exp claim is optional.")
-	flag.Bool(withIssuedAtFlag, false, "Enables verification of issued-at.")
-	flag.String(withIssuerFlag, "", "Require the specified issuer in the `iss` claim. Validation will fail if a different issuer is specified")
-	flag.Bool(withJSONNumberFlag, false, "Configures the underlying JSON parser with UseNumber.")
-	flag.Duration(withLeewayFlag, 0, "Specify the leeway window as duration e.g. 5s.")
-	flag.Bool(withPaddingAllowedFlag, false, "Enable the codec used for decoding JWTs to allow padding. Note that the JWS RFC7515 states that the tokens will utilize a Base64url encoding with no padding.")
-	flag.Bool(withStrictDecodingFlag, false, "Switch the codec used for decoding JWTs into strict mode. In this mode, the decoder requires that trailing padding bits are zero, as described in RFC 4648 section 3.5.")
-	flag.String(withSubjectFlag, "", "Configures the validator to require the specified subject in the `sub` claim.")
-	flag.StringArray(withValidMethodFlag, []string{}, "Supply algorithm methods that the parser will check.")
+	pflag.String(flags.WithAudience, "", "Configures the validator to require the specified audience in the `aud` claim.")
+	pflag.Bool(flags.WithExpirationRequired, false, "This makes the exp claim required. By default exp claim is optional.")
+	pflag.Bool(flags.WithIssuedAt, false, "Enables verification of issued-at.")
+	pflag.String(flags.WithIssuer, "", "Require the specified issuer in the `iss` claim. Validation will fail if a different issuer is specified")
+	pflag.Bool(flags.WithJSONNumber, false, "Configures the underlying JSON parser with UseNumber.")
+	pflag.Duration(flags.WithLeeway, 0, "Specify the leeway window as duration e.g. 5s.")
+	pflag.Bool(flags.WithPaddingAllowed, false, "Enable the codec used for decoding JWTs to allow padding. Note that the JWS RFC7515 states that the tokens will utilize a Base64url encoding with no padding.")
+	pflag.Bool(flags.WithStrictDecoding, false, "Switch the codec used for decoding JWTs into strict mode. In this mode, the decoder requires that trailing padding bits are zero, as described in RFC 4648 section 3.5.")
+	pflag.String(flags.WithSubject, "", "Configures the validator to require the specified subject in the `sub` claim.")
+	pflag.StringArray(flags.WithValidMethod, []string{}, "Supply algorithm methods that the parser will check.")
 
-	flag.StringP(keyFileFlag, "k", "", "Provide the path to a PEM file containing the Key used to sign the JWT.")
+	pflag.StringP(flags.KeyFile, "k", "", "Provide the path to a PEM file containing the Key used to sign the JWT. Otherwise signature can't be verified and will not be printed.")
 
-	flag.Usage = func() {
+	pflag.Usage = func() {
 		w := os.Stderr
 
 		fmt.Fprintf(w, "Usage of %s: \n", os.Args[0])
@@ -81,13 +59,13 @@ Examples:
   jotty <jwt token data>
   pbpaste | jotty -f - --no-header --no-signature
 
-Flags:`)
+s:`)
 
-		flag.PrintDefaults()
+		pflag.PrintDefaults()
 	}
 
-	flag.Parse()
-	err := viper.BindPFlags(flag.CommandLine)
+	pflag.Parse()
+	err := viper.BindPFlags(pflag.CommandLine)
 	eslog.LogIfError(err, eslog.Fatal)
 	err = viper.UnmarshalExact(conf)
 	eslog.LogIfError(err, eslog.Fatal)
@@ -97,11 +75,14 @@ func main() {
 	if conf.Version {
 		fmt.Printf("jotty version: %s\n", version)
 	} else if conf.Help {
-		flag.Usage()
+		pflag.Usage()
 	} else {
 		var jwtRaw string
+		var decodedJWT *jwt.Token
+		var err error
+
 		if len(conf.File) > 0 {
-			jwtBt, err := internal.ReadData(conf.File)
+			jwtBt, err := jotty.ReadData(conf.File)
 			eslog.LogIfError(err, eslog.Error)
 			jwtRaw = string(jwtBt)
 		} else {
@@ -119,16 +100,22 @@ func main() {
 		signingKey, err := conf.GetPublicKey()
 		eslog.LogIfError(err, eslog.Fatal)
 
-		decodedJWT, err := jwt.Parse(jwtRaw, func(token *jwt.Token) (interface{}, error) {
-			// TODO: verify token alge here https://pkg.go.dev/github.com/golang-jwt/jwt/v5#example-Parse-Hmac
-			return signingKey, nil
-		}, opts...)
+		parser := jwt.NewParser(opts...)
+
+		if conf.SigningKey == "" {
+			decodedJWT, _, err = parser.ParseUnverified(jwtRaw, jwt.MapClaims{})
+		} else {
+			decodedJWT, err = parser.Parse(jwtRaw, func(token *jwt.Token) (interface{}, error) {
+				return signingKey, nil
+			})
+		}
+
 		switch {
 		case decodedJWT == nil:
 			eslog.Fatal("Couldn't handle this token:", err)
 		case decodedJWT.Valid:
 			fmt.Println("JWT token is valid")
-		default:
+		case err != nil:
 			eslog.Error("Error parsing the JWT:", err)
 		}
 
@@ -157,15 +144,15 @@ func CheckError(err error, loggerFunc func(format string, args ...interface{})) 
 }
 
 func colorfulJsonEncode(data any) {
-	var enc *json.Encoder
+	var enc *jsoncolor.Encoder
 
-	if json.IsColorTerminal(os.Stdout) {
+	if jsoncolor.IsColorTerminal(os.Stdout) {
 		out := colorable.NewColorable(os.Stdout) // needed for Windows
-		enc = json.NewEncoder(out)
-		clrs := json.DefaultColors()
+		enc = jsoncolor.NewEncoder(out)
+		clrs := jsoncolor.DefaultColors()
 		enc.SetColors(clrs)
 	} else {
-		enc = json.NewEncoder(os.Stdout)
+		enc = jsoncolor.NewEncoder(os.Stdout)
 	}
 
 	enc.SetIndent("", "  ")
